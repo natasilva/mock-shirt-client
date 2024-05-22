9b4c0cf90782012565619e6a3f5a4b740d1259ad
AnthonyGabriel2025 <anthonygabriel2025@hotmail.com>
2024-05-22 15:19:41 -0300

    feat: Add formulario

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,18 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationService } from '../../core/services/navigation.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ColorService } from '../../core/services/color.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent {
-
+export class FormComponent implements OnInit { 
+  quantidadeLogos = 1;
+  colors$ = new Observable<any[]>()
+  
   constructor(
-    private navigationService: NavigationService
+    private formBuilder: FormBuilder,
+    private navigationService: NavigationService,
+    private colorService: ColorService
   ) {}
 
-  goToView () {
-    this.navigationService.navigate("/view")
+  ngOnInit(): void {
+    this.listColors()
   }
-}
+
+   listColors() {
+      this.colors$ =  this.colorService.list();
+      return this.colors$
+  }
+  
+
+  goToView () {
+
+  }
+  
+  increaseQuantity() {
+    if (this.quantidadeLogos < 3) {
+      this.quantidadeLogos++;
+    }
+  }
+
+  decreaseQuantity() {
+    if (this.quantidadeLogos > 1) {
+      this.quantidadeLogos--;
+    }
+  }
+  }
