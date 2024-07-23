b950807e5352801a4d8da0866546d2e0e9dfeb6a
natasilva <nata.software@hotmail.com>
2024-07-23 11:54:33 -0300

    feat: Add opcao para informar logo de cada manga individualmente

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -4,7 +4,6 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { ColorService } from '../../core/services/color.service';
 import { Observable } from 'rxjs';
 import { MaterialService } from '../../core/services/material.service';
-import { Router } from '@angular/router';
 import { ShirtService } from '../../core/services/shirt.service';
 
 @Component({
@@ -25,7 +24,6 @@ export class FormComponent implements OnInit {
     private colorService: ColorService,
     private materialService: MaterialService,
     private shirtService: ShirtService,
-    private router: Router
   ) {
   }
   
@@ -33,10 +31,32 @@ export class FormComponent implements OnInit {
     this.form = this.formBuilder.group({
       material: [null, Validators.required],
       color: [null, Validators.required],
-      file: ['', [Validators.required, this.logoFileValidator.bind(this)]],
+      backLogo: ['', [Validators.required, this.logoFileValidator.bind(this)]],
+      frontLogo: ['', [Validators.required, this.logoFileValidator.bind(this)]],
+      rightSleeveLogo: ['', null],
+      leftSleeveLogo: ['', null],
       sleeveLogo: [false, Validators.required],
       logoColorsQuantity: [1, [Validators.required, Validators.min(1), Validators.max(3)]]
     });
+
+    this.form.controls['sleeveLogo'].valueChanges.subscribe((val) => {
+      const rightSleeveControl = this.form.get('rightSleeveLogo');
+      const leftSleeveControl = this.form.get('leftSleeveLogo');
+
+      if (!val) {
+        rightSleeveControl.setValue(null);
+        leftSleeveControl.setValue(null);
+      
+        rightSleeveControl.clearValidators();
+        leftSleeveControl.clearValidators();
+      } else {
+        rightSleeveControl.setValidators([Validators.required, this.logoFileValidator.bind(this)]);
+        leftSleeveControl.setValidators([Validators.required, this.logoFileValidator.bind(this)]);
+      }
+
+      rightSleeveControl.updateValueAndValidity();
+      leftSleeveControl.updateValueAndValidity();
+    })
 
     this.listColors();
     this.listMaterials();
@@ -79,7 +99,7 @@ export class FormComponent implements OnInit {
   
   selectedFile: File | null = null;
 
-  onFileChange(event: Event) {
+  onFileChange(event: Event, control: string) {
     const inputElement = event.target as HTMLInputElement;
     const fileList: FileList | null = inputElement.files;
 
@@ -89,10 +109,10 @@ export class FormComponent implements OnInit {
   
       if (validTypes.includes(file.type)) {
         this.selectedFile = file;
-        this.form.get('file')!.setValue(file);
+        this.form.get(control)!.setValue(file);
       } else {
         this.selectedFile = null;
-        this.form.get('file')!.setValue(null);
+        this.form.get(control)!.setValue(null);
       }
     }
   }
@@ -104,11 +124,18 @@ export class FormComponent implements OnInit {
     }
 
     const formData = new FormData();
-    formData.append('material', this.form.get('material').value.key);
-    formData.append('color', this.form.get('color').value.key);
-    formData.append('file', this.form.get('file').value);
+
+    formData.append('material', this.form.get('material').value.acronym);
+    formData.append('color', this.form.get('color').value.acronym);
+    formData.append('frontLogo', this.form.get('frontLogo').value);
+    formData.append('backLogo', this.form.get('backLogo').value);    
     formData.append('sleeveLogo', this.form.get('sleeveLogo').value);
     formData.append('logoColorsQuantity', this.form.get('logoColorsQuantity').value);
+
+    if (this.form.get('sleeveLogo').value) {
+      formData.append('rightSleeveLogo', this.form.get('rightSleeveLogo').value);
+      formData.append('leftSleeveLogo', this.form.get('leftSleeveLogo').value);
+    }
 
     this.shirtService.processShirt(formData).subscribe({
       next: (response) => {
@@ -125,6 +152,18 @@ export class FormComponent implements OnInit {
         console.error('Erro ao processar a camiseta:', error);
       }
     });
+  }
+
+  getErrorMessage(controlName: string): string {
+    const control = this.form.get(controlName);
+  
+    if (control.hasError('required')) {
+      return 'Este campo é obrigatório!';
+    } else if (control.hasError('requiredFileType')) {
+      return 'Preencha todos campos obrigatórios! (jpg/png)';
+    }
+  
+    return '';
   }
   
 }
